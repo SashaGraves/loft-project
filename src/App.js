24b6b46f13cd22b8645cd205444e76de5473a998
SashaGraves <SashaGraves>
2020-09-09 11:31:31 +0300

    Navigation.
    Components added. PropTypes added. Navigation through state changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import './App.css';
-import Header from '~/src/component/Header/Header.js';
+import Header from 'component/Header.js';
+import Profile from 'component/Profile/Profile.js';
+import Map from 'component/Map/Map.js';
+import Login from 'component/Login/Login';
+import SignUp from 'component/SignUp/SignUp';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      type: "LOGIN",
+    }
+
+    this.changePage = this.changePage.bind(this);
+  }
+
+  changePage(type) {
+    this.setState({type});
+  } 
+
+  render() {
+    return (
     <div className="App">
-      <Header />
+      <Header changePage={this.changePage}/>
+      {
+        {
+          PROFILE: <Profile />,
+          MAP: <Map />,
+          LOGIN: <Login />,
+          SIGNUP: <SignUp />
+        }[this.state.type]
+      }
     </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,5 +1,6 @@
+import PropTypes from 'prop-types';
 import React from 'react';
-import logo from '../public/logo-taxi.svg';
+import logo from 'logo-taxi.svg';
 
 class Header extends React.Component {
     constructor(props) {
@@ -13,9 +14,19 @@ class Header extends React.Component {
         return (
             <header>
                 <img src={logo} />
+
+                <nav style={{listStyle: 'none',}}>
+                    <li><a href="#" onClick={() => this.props.changePage("MAP")}>Карта</a></li>
+                    <li><a href="#" onClick={() => this.props.changePage("PROFILE")}>Профиль</a></li>
+                    <li><a href="#" onClick={() => this.props.changePage("LOGIN")}>Логин</a></li>
+                </nav>
             </header>
         );
     }
 };
+
+Header.propTypes = {
+    changePage: PropTypes.func
+}
 
 export default Header;
