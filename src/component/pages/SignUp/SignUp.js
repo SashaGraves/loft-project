2fdc6841de3917d898f9af9ad98562ebf2042ec3
SashaGraves <SashaGraves>
2020-10-12 16:09:23 +0300

    Some fixes to Final-form

--- src/component/pages/SignUp/SignUp.js
+++ src/component/pages/SignUp/SignUp.js
@@ -29,189 +29,33 @@ const styles = {
             width: 156,
             marginTop: 150,
         },
-        button: {
-            backgroundColor: '#ffc617',
-            textTransform: 'capitalize',
-            float: 'right',
-            marginTop: 15,
-        },
-        textField: {
-            marginBottom: 15,
-        },
-        span: {
-            marginRight: 10,
-        }, 
-        form: {
-            width: 402,
-            margin: '0 auto'
-        },
     }
 
-const SignUp = ({isLoading, postRegisterInfo, credentialError, credentialMessage}) => {
-    
-    const [emailInput, setEmailInput] = useState('');
-    const [emailInputError, setEmailInputError] = useState('');
-    const [nameInput, setNameInput] = useState('');
-    const [nameInputError, setNameInputError] = useState('');
-    const [surnameInput, setSurnameInput] = useState('');
-    const [surnameInputError, setSurnameInputError] = useState('');
-    const [passwordInput, setPasswordInput] = useState('');
-    const [passwordInputError, setPasswordInputError] = useState('');
-    const [errorMessage, setErrorMessage] = useState(false);
-    
-    
-    const onSubmit = (e) => {
-        e.preventDefault();
-        if (nameInputError === 'success' && 
-            surnameInputError === 'success' &&
-            passwordInputError === 'success' &&
-            emailInputError === 'success') {
-                postRegisterInfo({
-                    email: emailInput,
-                    password: passwordInput,
-                    name: nameInput,
-                    surname: surnameInput,
-                });
-        } else {
-            setErrorMessage(true);
-        }
-    }
-
-    const onChangeEmail = (event) => {
-        setErrorMessage(false);
-        setEmailInput(event.target.value);
-        if (event.target.value) {
-            setEmailInputError('success');
-        } else {
-            setEmailInputError('error');
-        }
-    }
-
-    const onChangeName = (event) => {
-        setErrorMessage(false);
-        setNameInput(event.target.value);
-        if (event.target.value) {
-            setNameInputError('success');
-        } else {
-            setNameInputError('error');
-        }
-    }
-
-    const onChangeSurname = (event) => {
-        setErrorMessage(false);
-        setSurnameInput(event.target.value);
-        if (event.target.value) {
-            setSurnameInputError('success');
-        } else {
-            setSurnameInputError('error');
-        }
-    }
-
-    const onChangePassword = (event) => {
-        setErrorMessage(false);
-        setPasswordInput(event.target.value);
-        if (event.target.value) {
-            setPasswordInputError('success');  
-        } else {
-            setPasswordInputError('error');
-        }
-    }
-    
-
-    return (
+const SignUp = () => (
         <Grid container style={styles.gridContainer}>
-            <Grid item xs={12} />
-            <Grid item xs={4} />
-            <Grid item xs={2}>
-                <img src={logo} style={styles.logoImage} alt="logo Loft-taxi" />
-            </Grid>
-            <Grid item xs={4}>
-                <Paper style={styles.container}>
-                    <Typography variant='h4' align='left' color='textPrimary' gutterBottom>
-                        Регистрация
-                    </Typography>
-                    <Typography align='left' style={styles.typography}>
-                        Уже зарегистрированы?
-                        <Link to="/login" style={styles.link}>
-                            Войти
-                        </Link>
-                    </Typography>
-                    
-                    <SignUpForm />
-                    {/* <form onSubmit={onSubmit} style={styles.form}>
-                        <TextField
-                            id="email"
-                            name="email"
-                            label="Адрес электронной почты *"
-                            fullWidth
-                            style={styles.textField}
-                            value={emailInput}
-                            onChange={onChangeEmail}
-                            helperText={(emailInputError === 'error') && 'Введите email'}
-
-                        />
-                            
-                        <TextField
-                            id="name"
-                            name="name"
-                            label="Имя *"
-                            value={nameInput}
-                            onChange={onChangeName}
-                            helperText={(nameInputError === 'error') && 'Введите имя'}
-
-                        />
-
-                        <span style={styles.span}/>
-                        
-                        <TextField
-                            id="surname"
-                            name="surname"
-                            label="Фамилия *"
-                            style={styles.textField}
-                            value={surnameInput}
-                            onChange={onChangeSurname}
-                            helperText={(surnameInputError === 'error') && 'Введите фамилию'}
-                        />
-                        
-                        <TextField
-                            id="password"
-                            name="password"
-                            label="Пароль *"
-                            fullWidth
-                            style={styles.textField}
-                            value={passwordInput}
-                            onChange={onChangePassword}
-                            helperText={(passwordInputError === 'error') && 'Введите пароль'}
-                        />
-                        {errorMessage 
-                        && 
-                        <Typography color="error" variant="body1">Заполните все поля</Typography>}
-                        
-                            <Button variant="contained" type="submit" style={styles.button} disabled={isLoading}>Войти</Button>
-                    
-                    </form> */}
-                    {/* {credentialError && 
-                    <Typography color="error" variant="subtitle1">{credentialMessage}</Typography>} */}
-                </Paper>
-            </Grid>
-            <Grid item xs={2} />
+        <Grid item xs={12} />
+        <Grid item xs={4} />
+        <Grid item xs={2}>
+            <img src={logo} style={styles.logoImage} alt="logo Loft-taxi" />
         </Grid>
-    );
+        <Grid item xs={4}>
+            <Paper style={styles.container}>
+                <Typography variant='h4' align='left' color='textPrimary' gutterBottom>
+                    Регистрация
+                </Typography>
+                <Typography align='left' style={styles.typography}>
+                    Уже зарегистрированы?
+                    <Link to="/login" style={styles.link}>
+                        Войти
+                    </Link>
+                </Typography>
+                
+                <SignUpForm />
 
-}
-
-// const mapStateToProps = store => ({
-//     isLoading: store.isLoading,
-//     credentialError: store.credential.credentialError,
-//     credentialMessage: store.credential.credentialMessage,
-// });
-
-// SignUp.propTypes = {
-//     isLoading: PropTypes.bool.isRequired,
-//     postRegisterInfo: PropTypes.func.isRequired,
-//     credentialError: PropTypes.bool,
-//     credentialMessage: PropTypes.string,
-// };
+            </Paper>
+        </Grid>
+        <Grid item xs={2} />
+    </Grid>
+);
 
 export default SignUp;
-// export default connect(mapStateToProps, {postRegisterInfo})(SignUp);
